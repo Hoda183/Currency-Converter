15b1a26c88af830e745570f4b625ec558fbf030f
Hoda183 <63477691+Hoda183@users.noreply.github.com>
2020-09-21 02:13:43 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main() {
+  float pounds;
+
+  printf("Entre the number\n");
+  scanf("%f",&pounds);
+  float convertedpounds=pounds/15.70;
+  printf("%f",convertedpounds);
+
+  return 0;
+}
